91e41118f19fcbf2b9351d6039650879d0c07ee0
Laura Galis <lgalis@redhat.com>
2018-09-20 13:22:35 -0400

    Update Card Styling

--- src/PresentationalComponents/CatalogItems/CatalogItemShow.js
+++ src/PresentationalComponents/CatalogItems/CatalogItemShow.js
@@ -11,18 +11,22 @@ import { GridItem } from '@patternfly/react-core'
 
 
 const propLine = (prop, value) => {
-    return(<div key = {prop} className = "card_element">  {prop} : {value} </div>);
+    return(<div key = {prop} className = "card_element"> {value} </div>);
 };
 
 const defaultProperty = property => {
     return ['match', 'location', 'history', 'imageUrl', 'provider_id', 'hideModal', 'showModal'].includes(property)
-}
+};
+
+const toDisplayProperty = property => {
+  return ['description'].includes(property)
+};
 
 const propDetails = item => {
     let details = [];
 
     for (let property in item) {
-        if (item.hasOwnProperty(property) && !defaultProperty(property)) {
+        if (item.hasOwnProperty(property) && toDisplayProperty(property)) {
             if (item[property] && item[property] !== undefined) {
                 details.push(propLine(property, item[property].toString()));
             }
@@ -75,11 +79,12 @@ class CatalogItemShow extends React.Component {
             <ImageWithDefault src={this.props.imageUrl || CatItemSvg} defaultSrc={CatItemSvg} width="50" height="50" />
           </div>
           <div className="card_body">
+            <h>{this.props.name}</h>
+            <br/><br/>
             {itemDetails(this.props)}
           </div>
           <br/>
-          <div className="bottom-48">
-            {this.props.name}
+          <div className="bottom-24">
           </div>
         </div>
       </GridItem>
