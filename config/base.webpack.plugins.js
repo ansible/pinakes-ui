a1b12b569ed91d34845948271314bbc0b82fc39d
Jason Frey <fryguy9@gmail.com>
2018-10-24 10:20:05 -0400

    Allow setting topological target at build time.

--- config/base.webpack.plugins.js
+++ config/base.webpack.plugins.js
@@ -82,11 +82,17 @@ const CopyFilesWebpackPlugin = new (require('copy-webpack-plugin'))([
 ]);
 plugins.push(CopyFilesWebpackPlugin);
 
+/**
+ * Makes build-time env vars available to the client-side as constants
+ */
 const envPlugin = new webpack.DefinePlugin({
   'process.env.API_HOST': JSON.stringify(process.env.API_HOST),
   'process.env.API_PORT': JSON.stringify(process.env.API_PORT),
+  'process.env.BASE_PATH': JSON.stringify(process.env.BASE_PATH),
+  'process.env.TOPO_API_HOST': JSON.stringify(process.env.TOPO_API_HOST),
+  'process.env.TOPO_API_PORT': JSON.stringify(process.env.TOPO_API_PORT),
+  'process.env.TOPO_BASE_PATH': JSON.stringify(process.env.TOPO_BASE_PATH),
 });
-
 plugins.push(envPlugin);
 
 module.exports = { plugins };
