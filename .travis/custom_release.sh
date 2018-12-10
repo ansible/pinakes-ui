8a3c72f702e4fb5231b95b92ec847aee092e46fe
Jason <jfrey@redhat.com>
2018-12-10 13:10:34 -0500

    More changes for insights deployment

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-# Check if it is a pull request
-# If it is not a pull request, generate the deploy key
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-    echo -e "Pull Request, not pushing a build"
-    exit 0;
-else
-    openssl aes-256-cbc -K $encrypted_5d746cffa216_key -iv $encrypted_5d746cffa216_iv -in service-portal.enc -out service-portal -d
-    chmod 600 service-portal
-    eval `ssh-agent -s`
-    ssh-add service-portal
-fi
-
 # If current dev branch is master, push to build repo ci-beta
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
     .travis/release.sh "ci-stable"
