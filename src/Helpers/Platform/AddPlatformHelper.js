62b592f4a8680b8ce4014757193b157853a33ec6
Martin Maroši <marvusm.mmi@gmail.com>
2018-12-13 16:16:45 +0100

    Deleted unused reducer, actions and action types.

--- src/Helpers/Platform/AddPlatformHelper.js
+++
@@ -1,7 +0,0 @@
-import { getUserApi } from '../Shared/userLogin';
-
-const api = getUserApi();
-
-export function addPlatform(providerData) {
-  return api.addProvider(providerData).then(data => data, error => console.error(error));
-}
--- src/Utilities/store.js
+++ src/Utilities/store.js
@@ -3,7 +3,6 @@ import promiseMiddleware from 'redux-promise-middleware';
 import { ReducerRegistry, applyReducerHash } from '@red-hat-insights/insights-frontend-components';
 import reduxLogger from 'redux-logger';
 import thunk from 'redux-thunk';
-import addPlatformReducer, { addPlatformInitialState } from '../redux/reducers/addPlatformReducer';
 import alertReducer, { alertsInitialState } from '../redux/reducers/alertReducer';
 import mainModalReducer, { mainModalInitialState } from '../redux/reducers/mainModalReducer';
 import orderReducer, { orderInitialState } from '../redux/reducers/orderReducer';
@@ -14,7 +13,6 @@ import uiReducer, { uiInitialState } from '../redux/reducers/UiReducer';
 const registry = new ReducerRegistry({}, [ promiseMiddleware(), thunk, reduxLogger ]);
 
 registry.register({
-  addPlatformReducer: applyReducerHash(addPlatformReducer, addPlatformInitialState),
   alertReducer: applyReducerHash(alertReducer, alertsInitialState),
   mainModalReducer: applyReducerHash(mainModalReducer, mainModalInitialState),
   orderReducer: applyReducerHash(orderReducer, orderInitialState),
