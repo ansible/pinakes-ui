b4b6ab48148ab69ccc70823e01b77095b0ef7cd5
Martin Maroši <marvusm.mmi@gmail.com>
2019-01-03 09:15:01 +0100

    Removed default src from image with default component.

--- src/PresentationalComponents/Shared/ImageWithDefault.js
+++ src/PresentationalComponents/Shared/ImageWithDefault.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import DefaultPortfolioImg from '../../assets/images/default-portfolio.jpg';
 
 const ImageWithDefault = ({ src, ...props }) => <img src={ src } { ...props } />;
 
 ImageWithDefault.propTypes = {
-  src: propTypes.string
-};
-
-ImageWithDefault.defaultProps = {
-  src: DefaultPortfolioImg
+  src: propTypes.string.isRequired
 };
 
 export default ImageWithDefault;
