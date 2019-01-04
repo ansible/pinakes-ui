6762e6fedf290bdd49f9b7befa3110ff1deb4a27
Laura Galis <lgalis@redhat.com>
2019-01-04 16:22:10 -0500

    Connect the cart button to the Order list component

--- src/PresentationalComponents/Portfolio/PortfolioOrderToolbar.js
+++ src/PresentationalComponents/Portfolio/PortfolioOrderToolbar.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import { Toolbar, ToolbarGroup, ToolbarItem, Title, Button } from '@patternfly/react-core';
+import { Toolbar, ToolbarGroup, ToolbarItem, Title } from '@patternfly/react-core';
 import { css } from '@patternfly/react-styles';
 import spacingStyles from '@patternfly/patternfly-next/utilities/Spacing/spacing.css';
+import OrderToolbarItem from '../Shared/OrderToolbarItem';
 import '../../SmartComponents/Portfolio/portfolio.scss';
 import '../Shared/toolbarschema.scss';
 
@@ -13,14 +14,7 @@ const PortfolioOrderToolbar = ({ title }) => (
         { title && <Title size={ '2xl' }> { title }</Title> }
       </ToolbarItem>
     </ToolbarGroup>
-    <ToolbarGroup className={ 'pf-u-ml-auto-on-xl' }>
-      <ToolbarItem className={ css(spacingStyles.mrXL) }>
-        <Button variant="link" id="ordersButton">
-          <i className="fas fa-shopping-cart" aria-hidden="true"></i>
-            Orders
-        </Button>
-      </ToolbarItem>
-    </ToolbarGroup>
+    <OrderToolbarItem/>
   </Toolbar>
 );
 
--- src/PresentationalComponents/Shared/OrderToolbarItem.js
+++ src/PresentationalComponents/Shared/OrderToolbarItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { ToolbarGroup, ToolbarItem, Button } from '@patternfly/react-core';
 import { css } from '@patternfly/react-styles';
 import spacingStyles from '@patternfly/patternfly-next/utilities/Spacing/spacing.css';
@@ -7,10 +8,12 @@ import '../../SmartComponents/Portfolio/portfolio.scss';
 const OrderToolbarItem = () => (
   <ToolbarGroup className={ 'pf-u-ml-auto-on-xl' }>
     <ToolbarItem className={ css(spacingStyles.mrXL) }>
-      <Button variant="link" id="ordersButton">
-        <i className="fas fa-shopping-cart" aria-hidden="true"></i>
+      <Link key="show_order-list" to={ `/orders` }>
+        <Button variant="link" id="ordersButton">
+          <i className="fas fa-shopping-cart" aria-hidden="true"></i>
           Orders
-      </Button>
+        </Button>
+      </Link>
     </ToolbarItem>
   </ToolbarGroup>
 );
