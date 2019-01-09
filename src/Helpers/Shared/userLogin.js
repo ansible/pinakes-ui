d0c598f41323e4576f4e847f6577d480cfca468c
Martin Maroši <marvusm.mmi@gmail.com>
2019-01-09 14:49:38 +0100

    Replaced git api clients with npm packages.

--- src/Helpers/Shared/userLogin.js
+++ src/Helpers/Shared/userLogin.js
@@ -1,9 +1,9 @@
-let TopologicalInventory = require('topological_inventory');
-let ServicePortalApi = require('service_portal_api');
+import { AdminsApi } from 'service_portal_api';
+import { DefaultApi } from 'topological_inventory';
 
 // TODO - replace with actual login info when available
 let admin_hash = { 'x-rh-auth-identity': btoa(JSON.stringify({ identity: { is_org_admin: true }})) };
-const adminApi = new ServicePortalApi.AdminsApi();
+const adminApi = new AdminsApi();
 Object.assign(adminApi.apiClient.defaultHeaders, admin_hash);
 
 export function getUserApi() {
@@ -12,7 +12,7 @@ export function getUserApi() {
 //UserSecurity.username = 'YOUR USERNAME'
 //UserSecurity.password = 'YOUR PASSWORD'
 
-let userTopologicalApi = new TopologicalInventory.DefaultApi();
+let userTopologicalApi = new DefaultApi();
 
 export function getTopologicalUserApi() {
   return userTopologicalApi;
