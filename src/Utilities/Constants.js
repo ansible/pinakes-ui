444c206c752866e2a5ce3a5ae4f3142fb2ef9f4b
Martin Maroši <marvusm.mmi@gmail.com>
2019-01-10 16:10:35 +0100

    Added API client base path constats.

--- src/Utilities/Constants.js
+++ src/Utilities/Constants.js
@@ -1,2 +1,8 @@
 export const DEFAULT_PAGE_SIZE = 50;
+export const SOURCES_API_BASE = '/r/insights/platform/topological-inventory/v0.0'
 
+const apiHost = process.env.API_HOST || "localhost";
+const apiPort = process.env.API_PORT || "5000";
+const basePath = process.env.BASE_PATH || "";
+const apiVersion = process.env.API_VERSION || "/api/v0.0";
+export const SERVICE_PORTAL_API_BASE = `https://${apiHost}:${apiPort}${basePath}${apiVersion.replace(/\/+$/, '')}`;
