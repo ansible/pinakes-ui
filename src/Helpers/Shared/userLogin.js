ddb7ab90f1b46b272fdcb631a98af99d269ba58b
Martin Maroši <marvusm.mmi@gmail.com>
2019-01-10 16:12:07 +0100

    Replaced api imports in api helpers.

--- src/Helpers/Shared/userLogin.js
+++ src/Helpers/Shared/userLogin.js
@@ -1,6 +1,6 @@
-import { AdminsApi } from 'service_portal_api';
-import { DefaultApi } from 'topological_inventory';
-
+import { AdminsApi, ApiClient as ServicePortalApiClient } from '@manageiq/service-portal-api';
+import { DefaultApi, ApiClient as TopologicalInventoryApiClient } from '@manageiq/topological_inventory';
+import { SOURCES_API_BASE, SERVICE_PORTAL_API_BASE } from '../../Utilities/Constants';
 // TODO - replace with actual login info when available
 let admin_hash = { 'x-rh-auth-identity': btoa(JSON.stringify({ identity: { is_org_admin: true }})) };
 const adminApi = new AdminsApi();
@@ -11,6 +11,12 @@ export function getUserApi() {
 }
 //UserSecurity.username = 'YOUR USERNAME'
 //UserSecurity.password = 'YOUR PASSWORD'
+
+const defaultClient = TopologicalInventoryApiClient.instance;                                    
+defaultClient.basePath = SOURCES_API_BASE;
+
+const sspDefaultClient = ServicePortalApiClient.instance
+sspDefaultClient.basePath = SERVICE_PORTAL_API_BASE;
 
 let userTopologicalApi = new DefaultApi();
 
