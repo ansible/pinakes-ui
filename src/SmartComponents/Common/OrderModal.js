1661a7f9590d0cf33d488711f88dbc9f88f57ebf
Laura Galis <lgalis@redhat.com>
2019-01-23 15:11:52 -0500

    Remove the use of PF3 Wizard component

--- src/SmartComponents/Common/OrderModal.js
+++ src/SmartComponents/Common/OrderModal.js
@@ -1,48 +1,23 @@
 import React, { Component } from 'react';
-import { Button, Grid, Modal} from '@patternfly/react-core';
+import { Button, Grid, Form, Title } from '@patternfly/react-core';
 import { connect } from 'react-redux';
 import './orderservice.scss';
 import propTypes from 'prop-types';
 import { OrderServiceFormSteps } from '../Order/OrderServiceFormConstants';
 import CatItemSvg from '../../assets/images/vendor-openshift.svg';
 import ImageWithDefault from '../../PresentationalComponents/Shared/ImageWithDefault';
-import { Wizard } from 'patternfly-react';
-
-// useless
-const renderStepWizardPage = (componentPage, props) => {
-  const StepComponent = componentPage;
-  return (<StepComponent { ...props } />);
-};
 
 class OrderModal extends Component {
   state = {
+    serviceData: {},
     activeStepIndex: 0
   };
 
-  onNext = () => {
-    const { activeStepIndex } = this.state;
-    const numberSteps = OrderServiceFormSteps.length;
+  onNext = () => this.setState((prevState, props) => ({ activeStepIndex: prevState.activeStepIndex + 1 }));
 
-    if (activeStepIndex < numberSteps - 1) {
-      this.setState({ activeStepIndex: activeStepIndex + 1 });
-    }
-  }
-
-  renderWizardSteps = () => {
-    const { activeStepIndex } = this.state;
-    const wizardSteps = OrderServiceFormSteps;
-    const activeStep = wizardSteps[activeStepIndex];
-
-    return wizardSteps.map((step, stepIndex) => (
-      <Wizard.Step
-        key={ stepIndex }
-        stepIndex={ stepIndex }
-        step={ step.step }
-        label={ step.label }
-        title={ step.title }
-        activeStep={ activeStep && activeStep.step }
-      />
-    ));
+  renderStepPage = (componentPage, props) => {
+    const StepComponent = componentPage;
+    return (<StepComponent { ...props } />);
   };
 
   render() {
@@ -53,36 +28,21 @@ class OrderModal extends Component {
     }
 
     const { activeStepIndex } = this.state;
-    const wizardSteps = OrderServiceFormSteps;
+    const steps = OrderServiceFormSteps;
 
     return (
-      <div>
-        <div>
+      <React.Fragment>
+        <Title>
           <ImageWithDefault src = { this.props.servicedata.imageUrl || CatItemSvg } width="40" />
-          { this.props.servicedata.name }}
-        </div>
-        <Wizard.Steps steps={ this.renderWizardSteps(this.props.servicedata) }/>
-        <Wizard.Row>
-          <Wizard.Main>
-            <Grid>
-              { wizardSteps.map((step, stepIndex) => (
-                <Wizard.Contents key={ step.title } stepIndex={ stepIndex } activeStepIndex={ activeStepIndex }>
-                  { wizardSteps[stepIndex].page, this.props.servicedata }
-                </Wizard.Contents>
-              )) }
-              { activeStepIndex !== wizardSteps.length - 1 && (
-                <div>
-                  <br/>
-                  <br/>
-                  <Button variant="primary" type="button" onClick={ this.onNext }>
-                    Order
-                  </Button>
-                </div>
-              ) }
-            </Grid>
-          </Wizard.Main>
-        </Wizard.Row>
-      </div>
+          { this.props.servicedata.name }
+        </Title>
+        <Grid>
+          { this.renderStepPage(steps[activeStepIndex].page, this.props.servicedata) }
+          { (activeStepIndex < steps.length - 1) && <Button variant="primary" type="button" onClick={ this.onNext }>
+            Order
+          </Button> }
+        </Grid>
+      </React.Fragment>
     );
   }
 }
