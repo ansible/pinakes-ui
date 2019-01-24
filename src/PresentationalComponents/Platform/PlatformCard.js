a1fa765063018606cb25e4bb292ee3eabcc22385
Martin Maroši <marvusm.mmi@gmail.com>
2019-01-24 09:20:54 +0100

    Added link to platform cards.

--- src/PresentationalComponents/Platform/PlatformCard.js
+++ src/PresentationalComponents/Platform/PlatformCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import './platformcard.scss';
 import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { GridItem, Card, CardHeader, CardBody, CardFooter } from '@patternfly/react-core';
 import DefaultPlatformImg from '../../assets/images/platform-default.svg';
 import OpenshiftPlatformImg from '../../assets/images/platform-openshift.svg';
 import AmazonPlatformImg from '../../assets/images/platform-amazon.png';
 import ImageWithDefault from '../Shared/ImageWithDefault';
 import ItemDetails from '../Shared/CardCommon';
-import { GridItem, Card, CardHeader, CardBody, CardFooter } from '@patternfly/react-core';
+import './platformcard.scss';
 
 const TO_DISPLAY = [ 'description', 'modified' ];
 
@@ -24,16 +25,18 @@ const platformTypeImg = (sourceTypeId) => {
 
 const PlatformCard = ({ name, id, ...props }) => (
   <GridItem sm={ 6 } md={ 4 } lg={ 4 } xl={ 3 }>
-    <Card className="card_style" key={ id }>
-      <CardHeader className="pcard_header">
-        <ImageWithDefault src={ platformTypeImg(props.source_type_id || '0') } defaultSrc={ DefaultPlatformImg } width="80" height="40"/>
-      </CardHeader>
-      <CardBody className="pcard_body">
-        <h4>{ name }</h4>
-        <ItemDetails { ...{ name, ...props } } toDisplay={ TO_DISPLAY } />
-      </CardBody>
-      <CardFooter/>
-    </Card>
+    <Link to={ `/platform/${id}` } className="card-link">
+      <Card key={ id }>
+        <CardHeader className="pcard_header">
+          <ImageWithDefault src={ platformTypeImg(props.source_type_id || '0') } defaultSrc={ DefaultPlatformImg } width="80" height="40"/>
+        </CardHeader>
+        <CardBody className="pcard_body">
+          <h4>{ name }</h4>
+          <ItemDetails { ...{ name, ...props } } toDisplay={ TO_DISPLAY } />
+        </CardBody>
+        <CardFooter/>
+      </Card>
+    </Link>
   </GridItem>
 );
 
