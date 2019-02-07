2e63afe9ed631b3256a32f56442e8a074d8b65cf
Martin Maroši <marvusm.mmi@gmail.com>
2019-02-07 14:27:15 +0100

    update tolopological inventory API to version 0.1.0.

--- src/Utilities/Constants.js
+++ src/Utilities/Constants.js
@@ -1,2 +1,2 @@
 export const SERVICE_PORTAL_API_BASE = `${process.env.BASE_PATH}/service-portal/v0.1`;
-export const TOPOLOGICAL_INVENTORY_API_BASE = `${process.env.BASE_PATH}/topological-inventory/v0.0`;
+export const TOPOLOGICAL_INVENTORY_API_BASE = `${process.env.BASE_PATH}/topological-inventory/v0.1`;
