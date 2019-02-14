3e513b2ac835c68774d6aeea0280e106a82b5d08
Martin Maroši <marvusm.mmi@gmail.com>
2019-02-14 14:59:38 +0100

    Created common card body for service offering cards.

---
+++ src/PresentationalComponents/Shared/service-offering-body.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { CardBody, Text, TextContent, TextVariants } from '@patternfly/react-core';
+import ItemDetails from './CardCommon';
+
+const ServiceOfferingCardBody = ({ name, display_name, distributor, ...props }) =>(
+  <CardBody style={ { height: 240 } }>
+    <TextContent>
+      <Text
+        className="elipsis-text-overflow"
+        component={ TextVariants.h3 }
+        title={ display_name || name }
+      >
+        { display_name || name }
+      </Text>
+      <Text component={ TextVariants.small }>{ distributor }&nbsp;</Text>
+    </TextContent>
+    <ItemDetails { ...props } toDisplay={ [ props.long_description ? 'long_description' : 'description' ] } />
+  </CardBody>
+);
+
+ServiceOfferingCardBody.propTypes = {
+  name: PropTypes.string,
+  display_name: PropTypes.string,
+  distributor: PropTypes.string,
+  long_description: PropTypes.string
+};
+
+export default ServiceOfferingCardBody;
