21d3181826ea60de7b6cc4486d346dc4db69ab48
Jason Frey <jfrey@redhat.com>
2019-03-14 22:25:39 -0400

    Change .travis/promote.sh to allow for CI and local promotion

--- .travis/promote.sh
+++ .travis/promote.sh
@@ -1,14 +1,35 @@
 #!/usr/bin/env bash
-set -x
+
+if [ "${CI}" = "true" ]; then
+  cd "${TRAVIS_BUILD_DIR}"
+  REMOTE=https://${GITHUB_PROMOTION_AUTH}@github.com/${TRAVIS_REPO_SLUG}.git
+  SOURCE=${TRAVIS_BRANCH}
+else
+  REMOTE=${2:-upstream}
+  SOURCE=$(git symbolic-ref --short HEAD 2>/dev/null || git rev-parse HEAD)
+fi
+
+DEST=$1
+if [ -z "${DEST}" ]; then
+  echo "Usage: $0 dest-branch [dest-remote]"
+  exit 1
+fi
+
+if ! git diff-index --quiet HEAD -- ; then
+  echo "Cannot promote with a dirty working tree."
+  exit 1
+fi
+
+echo "Promoting ${SOURCE} to ${DEST}..."
 set -e
+set -x
 
-echo "Automatically promoting $1 to ${TRAVIS_BRANCH}..."
+git checkout "${DEST}" || git checkout -b "${DEST}"
+git merge --no-ff --no-edit "${SOURCE}"
+{ set +x; } 2>/dev/null
+echo "+ git push ${DEST}"
+git push "${REMOTE}" "${DEST}" &>/dev/null
 
-cd "${TRAVIS_BUILD_DIR}"
-git clean -xdf
-git checkout -B "$1"
-git merge --no-ff --no-edit "${TRAVIS_BRANCH}"
-set +x
-echo "git push origin ${TRAVIS_BRANCH}:#{$1}"
-git push https://${GITHUB_PROMOTION_AUTH}@github.com/${TRAVIS_REPO_SLUG}.git "$1" &> /dev/null
-echo $?
+if [ ! "${CI}" = "true" ]; then
+  git checkout -
+fi
