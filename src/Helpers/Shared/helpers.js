3461cd39929396bed5324de3e8ec1e6303fb4057
Martin Marosi <marvusm.mmi@gmail.com>
2019-03-22 09:28:15 +0100

    Added allowNull proptype check.

--- src/Helpers/Shared/helpers.js
+++ src/Helpers/Shared/helpers.js
@@ -8,3 +8,11 @@ export const filterServiceOffering = ({ display_name, name }, filter) => {
   const filterAtrribute = display_name || name;
   return filterAtrribute.trim().toLowerCase().includes(filter.toLowerCase());
 };
+
+export const allowNull = wrappedPropTypes => (props, propName, ...rest) => {
+  if (props[propName] === null) {
+    return null;
+  }
+
+  return wrappedPropTypes(props, propName, ...rest);
+};
