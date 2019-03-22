fc0f7406d862fac80431bbf323c67f6f0214ca97
Martin Marosi <marvusm.mmi@gmail.com>
2019-03-22 09:31:53 +0100

    Added tests for portfolio item editing.

---
+++ src/test/smart-components/portfolio/portfolio-item-detail/portfolio-item-detail-toolbar.test.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import thunk from 'redux-thunk';
+import { shallow, mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store' ;
+import { shallowToJson } from 'enzyme-to-json';
+import { MemoryRouter, Route } from 'react-router-dom';
+import promiseMiddleware from 'redux-promise-middleware';
+import { notificationsMiddleware } from '@red-hat-insights/insights-frontend-components/components/Notifications';
+
+import EditToolbarActions from '../../../../SmartComponents/Portfolio/portfolio-item-detail/edit-toolbar-actions';
+import DetailToolbarActions from '../../../../SmartComponents/Portfolio/portfolio-item-detail/detail-toolbar-actions';
+import PortfolioItemDetailToolbar from '../../../../SmartComponents/Portfolio/portfolio-item-detail/portfolio-item-detail-toolbar';
+
+describe('<PortfolioItemDetailToolbar />', () => {
+  let initialProps;
+  let initialState;
+  const middlewares = [ thunk, promiseMiddleware(), notificationsMiddleware() ];
+  let mockStore;
+
+  const ComponentWrapper = ({ store, children, initialEntries = []}) => (
+    <Provider store={ store }>
+      <MemoryRouter initialEntries={ initialEntries }>
+        { children }
+      </MemoryRouter>
+    </Provider>
+  );
+
+  beforeEach(() => {
+    initialProps = {
+      url: '/foo',
+      isOpen: false,
+      product: {
+        name: 'bar'
+      },
+      setOpen: jest.fn(),
+      setWorkflow: jest.fn(),
+      handleUpdate: jest.fn()
+    };
+
+    initialState = {};
+    mockStore = configureStore(middlewares);
+  });
+
+  it('should render correctly', () => {
+    const wrapper = shallow(
+      <MemoryRouter>
+        <PortfolioItemDetailToolbar { ...initialProps } />
+      </MemoryRouter>
+    );
+    expect(shallowToJson(wrapper.find(PortfolioItemDetailToolbar))).toMatchSnapshot();
+  });
+
+  it('should render view variant', () => {
+    const store = mockStore(initialState);
+    const wrapper = mount(
+      <ComponentWrapper store={ store } initialEntries={ [ '/foo' ] }>
+        <Route path="/foo" render={ () => <PortfolioItemDetailToolbar { ...initialProps } /> } />
+      </ComponentWrapper>
+    );
+    expect(wrapper.find(DetailToolbarActions)).toHaveLength(1);
+  });
+
+  it('should render edit variant', () => {
+    const store = mockStore(initialState);
+    const wrapper = mount(
+      <ComponentWrapper store={ store } initialEntries={ [ '/foo/edit' ] }>
+        <Route path="/foo" render={ () => <PortfolioItemDetailToolbar { ...initialProps } /> } />
+      </ComponentWrapper>
+    );
+    expect(wrapper.find(EditToolbarActions)).toHaveLength(1);
+  });
+});
