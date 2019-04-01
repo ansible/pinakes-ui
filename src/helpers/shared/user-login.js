e13fdd554dbf49ca9e3e4eec2595ca187b519712
Martin Marosi <marvusm.mmi@gmail.com>
2019-04-01 11:33:09 +0200

    Use axios based approval client.

--- src/helpers/shared/user-login.js
+++ src/helpers/shared/user-login.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
-import { DefaultApi, ApiClient as TopologicalInventoryApiClient } from '@manageiq/topological_inventory';
-import { TOPOLOGICAL_INVENTORY_API_BASE, CATALOG_API_BASE } from '../../utilities/constants';
-
+import { WorkflowApi } from '@redhat-cloud-services/approval-client';
 import { AdminsApi as CatalogAdminsApi } from '@redhat-cloud-services/catalog-client';
+import { DefaultApi, ApiClient as TopologicalInventoryApiClient } from '@manageiq/topological_inventory';
+
+import { TOPOLOGICAL_INVENTORY_API_BASE, CATALOG_API_BASE, APPROVAL_API_BASE } from '../../utilities/constants';
 
 const axiosInstance = axios.create();
 
@@ -11,6 +12,7 @@ const resolveInterceptor = response => response.data || response;
 axiosInstance.interceptors.response.use(resolveInterceptor);
 
 const catalogAdmin = new CatalogAdminsApi(undefined, CATALOG_API_BASE, axiosInstance);
+const workflowApi = new WorkflowApi(undefined, APPROVAL_API_BASE, axiosInstance);
 
 const defaultClient = TopologicalInventoryApiClient.instance;
 defaultClient.basePath = TOPOLOGICAL_INVENTORY_API_BASE;
@@ -23,4 +25,8 @@ export function getTopologicalUserApi() {
 
 export function getUserApi() {
   return catalogAdmin;
+}
+
+export function getWorkflowApi() {
+  return workflowApi;
 }
