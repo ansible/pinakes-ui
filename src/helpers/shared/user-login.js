c1fe8635e52b1b53c01980e0129c431f5b70db59
Martin Marosi <marvusm.mmi@gmail.com>
2019-04-08 17:16:06 +0200

    Updated catalog js client.

--- src/helpers/shared/user-login.js
+++ src/helpers/shared/user-login.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { WorkflowApi } from '@redhat-cloud-services/approval-client';
-import { AdminsApi as CatalogAdminsApi } from '@redhat-cloud-services/catalog-client';
+import { PortfolioApi, PortfolioItemApi, OrderApi } from '@redhat-cloud-services/catalog-client';
 import { DefaultApi, ApiClient as TopologicalInventoryApiClient } from '@manageiq/topological_inventory';
 
 import { TOPOLOGICAL_INVENTORY_API_BASE, CATALOG_API_BASE, APPROVAL_API_BASE, RBAC_API_BASE } from '../../utilities/constants';
@@ -12,7 +12,9 @@ const resolveInterceptor = response => response.data || response;
 
 axiosInstance.interceptors.response.use(resolveInterceptor);
 
-const catalogAdmin = new CatalogAdminsApi(undefined, CATALOG_API_BASE, axiosInstance);
+const portfolioApi = new PortfolioApi(undefined, CATALOG_API_BASE, axiosInstance);
+const portfolioItemApi = new PortfolioItemApi(undefined, CATALOG_API_BASE, axiosInstance);
+const orderApi = new OrderApi(undefined, CATALOG_API_BASE, axiosInstance);
 const workflowApi = new WorkflowApi(undefined, APPROVAL_API_BASE, axiosInstance);
 
 const defaultClient = TopologicalInventoryApiClient.instance;
@@ -24,8 +26,16 @@ export function getTopologicalUserApi() {
   return userTopologicalApi;
 }
 
-export function getUserApi() {
-  return catalogAdmin;
+export function getPortfolioApi() {
+  return portfolioApi;
+}
+
+export function getPortfolioItemApi() {
+  return portfolioItemApi;
+}
+
+export function getOrderApi() {
+  return orderApi;
 }
 
 const defaultRbacClient = ApiClient.instance;
