dc577879c5a8b51f929b3eaa247aadc8434de0c3
Martin Marosi <marvusm.mmi@gmail.com>
2019-10-10 11:43:04 +0200

    Updated updatePortfolioItem to use JS client call.

--- src/helpers/portfolio/portfolio-helper.js
+++ src/helpers/portfolio/portfolio-helper.js
@@ -92,7 +92,7 @@ export function fetchProviderControlParameters(portfolioItemId) {
 }
 
 export async function updatePortfolioItem(portfolioItem) {
-  return await axiosInstance.patch(`${CATALOG_API_BASE}/portfolio_items/${portfolioItem.id}`, udefinedToNull(portfolioItem, PORTFOLIO_ITEM_NULLABLE));
+  return await portfolioItemApi.updatePortfolioItem(portfolioItem.id, udefinedToNull(portfolioItem, PORTFOLIO_ITEM_NULLABLE));
 }
 
 export function fetchPortfolioByName(name = '') {
