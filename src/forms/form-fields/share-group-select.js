aa1c5cbe0ea664a826ca019313bffad9cbb6059f
Martin Marosi <marvusm.mmi@gmail.com>
2019-11-04 10:12:07 +0100

    Updated share portfolio modal to look more like mocks.

---
+++ src/forms/form-fields/share-group-select.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { rawComponents } from '@data-driven-forms/pf4-component-mapper';
+import { Level, LevelItem } from '@patternfly/react-core';
+
+export const ShareGroupSelect = ({
+  FieldProvider,
+  inputName,
+  selectName,
+  groups,
+  permissions
+}) => {
+  return (
+    <div className="share-group-modal">
+      <Level>
+        <LevelItem className="share-column">
+          <FieldProvider name={ inputName } options={ groups }>
+            { ({ input, ...props }) => <rawComponents.Select isSearcheable isClearable placeholder="Select group" { ...input } { ...props } /> }
+          </FieldProvider>
+
+        </LevelItem>
+        <LevelItem className="share-column">
+          <FieldProvider name={ selectName } options={ permissions }>
+            { ({ input, ...props }) => <rawComponents.Select placeholder="Select permission" { ...input } { ...props } /> }
+          </FieldProvider>
+        </LevelItem>
+      </Level>
+    </div>
+  );
+};
+
+ShareGroupSelect.propTypes = {
+  FieldProvider: PropTypes.node.isRequired,
+  inputName: PropTypes.string.isRequired,
+  selectName: PropTypes.string.isRequired,
+  groups: PropTypes.any,
+  permissions: PropTypes.any
+};
+
+export default ShareGroupSelect;
+
--- src/smart-components/common/form-renderer.js
+++ src/smart-components/common/form-renderer.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import ReactFormRender, { componentTypes } from '@data-driven-forms/react-form-renderer';
 import { layoutMapper, formFieldsMapper } from '@data-driven-forms/pf4-component-mapper';
 import Pf4SelectWrapper from '../../presentational-components/shared/pf4-select-wrapper';
+import ShareGroupSelect from '../../forms/form-fields/share-group-select';
+import ShareGroupEdit from '../../forms/form-fields/shage-group-edit';
 
 const buttonPositioning = {
   default: {},
@@ -18,7 +20,9 @@ const FormRenderer = ({ componentMapper, formContainer, ...rest }) => (
       formFieldsMapper={ {
         ...formFieldsMapper,
         ...componentMapper,
-        [componentTypes.SELECT]: Pf4SelectWrapper
+        [componentTypes.SELECT]: Pf4SelectWrapper,
+        'share-group-select': ShareGroupSelect,
+        'share-group-edit': ShareGroupEdit
       } }
       layoutMapper={ layoutMapper }
       { ...buttonPositioning[formContainer] }
