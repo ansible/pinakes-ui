dc62615d4a9a41437ec508fa28ab94039f6ee94b
Laura Galis <lgalis@redhat.com>
2019-11-14 11:56:45 -0500

    Updated tests and snapshots

--- src/test/smart-components/platform/platform.test.js
+++ src/test/smart-components/platform/platform.test.js
@@ -7,44 +7,49 @@ import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 import Platform from '../../../smart-components/platform/platform';
 import { platformInitialState } from '../../../redux/reducers/platform-reducer';
+import { Provider } from 'react-redux';
+import { Route, MemoryRouter } from 'react-router-dom';
 
 describe('<Platform />', () => {
   let initialProps;
   const middlewares = [ thunk, promiseMiddleware(), notificationsMiddleware() ];
   let mockStore;
-  let intialState;
+  let initialState;
+
+  const ComponentWrapper = ({ store, initialEntries = [ '/foo' ], children }) => (
+    <Provider store={ store }>
+      <MemoryRouter initialEntries={ initialEntries }>
+        <Route path='/platforms'>
+          { children }
+        </Route>
+      </MemoryRouter>
+    </Provider>
+  );
 
   beforeEach(() => {
-    initialProps = {
-      match: {
-        params: {
-          id: 1
-        }
-      }
-    };
+    initialProps = {};
     mockStore = configureStore(middlewares);
-    intialState = {
+    initialState = {
       platformReducer: {
         ...platformInitialState,
         selectedPlatform: {
           id: '1',
           name: 'Foo'
         },
-        platformItems: {
-          1: {
-            meta: {
-              limit: 50,
-              offset: 0,
-              count: 0
-            }
-          }
-        }
+        platforms: [{
+          id: '1',
+          name: 'Foo'
+        }]
       }
     };
   });
 
   it('should render correctly', () => {
-    const wrapper = shallow(<Platform store={ mockStore(intialState) } { ...initialProps } />);
+    const store = mockStore(initialState);
+    const wrapper = shallow(
+      <ComponentWrapper store={ store } initialEntries={ [ '/platforms' ] }>
+        <Platform store={ mockStore(initialState) } { ...initialProps } />);
+      </ComponentWrapper>).find(Platform);
     expect(shallowToJson(wrapper)).toMatchSnapshot();
   });
 });
