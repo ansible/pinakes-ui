8a8e9eb93dfccbe829e1ead2d524ade9ad4bb3d8
Laura Galis <lgalis@redhat.com>
2019-11-20 10:33:48 -0500

    Fix the copy portfolio alert message

--- src/redux/actions/portfolio-actions.js
+++ src/redux/actions/portfolio-actions.js
@@ -203,7 +203,7 @@ export const copyPortfolio = id => dispatch => {
     dispatch({ type: 'COPY_PORTFOLIO_FULFILLED' });
     dispatch({ type: ADD_NOTIFICATION, payload: { variant: 'success',
       title: 'You have successfully copied a portfolio',
-      description: `${portfolio.name} has been copied.}`,
+      description: `${portfolio.name} has been copied.`,
       dismissable: true }});
     return portfolio;
   })
