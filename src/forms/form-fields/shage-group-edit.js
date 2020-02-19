6d00ccff66d7ab9bd1843049e12d432d463abd14
Martin Marosi <marvusm.mmi@gmail.com>
2020-02-19 08:41:07 +0100

    Use grid to enforce fixed width in share modal.

--- src/forms/form-fields/shage-group-edit.js
+++ src/forms/form-fields/shage-group-edit.js
@@ -2,31 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { rawComponents } from '@data-driven-forms/pf4-component-mapper';
 import {
-  Level,
-  LevelItem,
   Text,
   TextContent,
-  TextVariants
+  TextVariants,
+  GridItem,
+  Grid
 } from '@patternfly/react-core';
 
 const ShareGroupEdit = ({ FieldProvider, label, ...props }) => {
   return (
-    <div className="share-group-modal group">
-      <Level>
-        <LevelItem>
-          <TextContent>
-            <Text component={TextVariants.h4}>{label}</Text>
-          </TextContent>
-        </LevelItem>
-        <LevelItem className="share-column">
-          <FieldProvider {...props}>
-            {({ input, ...props }) => (
-              <rawComponents.Select {...input} {...props} />
-            )}
-          </FieldProvider>
-        </LevelItem>
-      </Level>
-    </div>
+    <Grid gutter="md" className="share-column">
+      <GridItem span={7}>
+        <TextContent>
+          <Text component={TextVariants.h4}>{label}</Text>
+        </TextContent>
+      </GridItem>
+      <GridItem span={5}>
+        <FieldProvider
+          {...props}
+          menuIsPortal
+          render={({ input, ...props }) => (
+            <rawComponents.Select {...input} {...props} />
+          )}
+        />
+      </GridItem>
+    </Grid>
   );
 };
 
