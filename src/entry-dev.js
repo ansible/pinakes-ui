472a6333a2ccb81df1383e2c6efd84c70326f177
Martin Maroši <marvusm.mmi@gmail.com>
2020-07-02 09:14:38 +0200

    Added simple console mark to entry-dev

--- src/entry-dev.js
+++ src/entry-dev.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import store from './utilities/store';
 import Router from './router';
 
+console.log('%c Catalog UI started in development mode', 'color: blue');
+
 ReactDOM.render(
   <Provider store={store()}>
     <Router />
