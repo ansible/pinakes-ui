52fcb0c7723b5eea7260731e8b775b60a3c82870
Greg McCullough <gmccullo@redhat.com>
2020-07-09 13:03:08 -0400

    Update custom_release.sh

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -5,7 +5,7 @@ NODE_ENV=production npm run build
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
     .travis/release.sh "ci-beta"
     .travis/release.sh "qa-beta"
-elif [ "${TRAVIS_BRANCH}" = "master-stable" ]; then
+elif [ "${TRAVIS_BRANCH}" = "stable" ]; then
     .travis/release.sh "ci-stable"
     .travis/release.sh "qa-stable"
 elif [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
