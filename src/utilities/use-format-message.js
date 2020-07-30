1c85c37215b9c4637ef64f9b806a909990ad8849
Martin Maroši <marvusm.mmi@gmail.com>
2020-07-30 13:50:48 +0200

    Add wrapper to rich text translation to avoid react key error.

--- src/utilities/use-format-message.js
+++ src/utilities/use-format-message.js
@@ -1,12 +1,24 @@
+import React, { Fragment } from 'react';
 const { useIntl } = require('react-intl');
 
 const useFormatMessage = () => {
   const { formatMessage } = useIntl();
-  return (message, values) => {
+  return (message, values = {}) => {
+    const internalValues = Object.entries(values).reduce(
+      (acc, [key, value]) => {
+        return {
+          ...acc,
+          [key]:
+            typeof value === 'function'
+              ? (chunks) => <Fragment key={key}>{value(chunks)}</Fragment>
+              : value
+        };
+      },
+      {}
+    );
     try {
-      return formatMessage(message, values);
+      return formatMessage(message, internalValues);
     } catch (error) {
-      console.error(error);
       return `Unable to translate message. Definition: ${message}, values: ${values}, intl-error: ${error}`;
     }
   };
