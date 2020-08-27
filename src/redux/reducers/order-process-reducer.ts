20f5c810270ee6b9292304558f4180730a1dd790
Martin Maroši <marvusm.mmi@gmail.com>
2020-08-27 10:55:49 +0200

    Convert order process reducer to TS.

---
+++ src/redux/reducers/order-process-reducer.ts
@@ -0,0 +1,89 @@
+import {
+  FETCH_ORDER_PROCESSES,
+  SORT_ORDER_PROCESSES,
+  SET_FILTER_ORDER_PROCESSES
+} from '../action-types';
+import {
+  ApiCollectionResponse,
+  AnyObject,
+  ReduxActionHandler,
+  ApiMetadata
+} from '../../types/common-types';
+import { OrderProcess } from '@redhat-cloud-services/catalog-client';
+
+export interface OrderProcessReducerState extends AnyObject {
+  orderProcesses: ApiCollectionResponse<OrderProcess>;
+  isLoading: boolean;
+  selectedOrderProcesses: string[];
+}
+
+export type OrderProcessReducerActionHandler = ReduxActionHandler<
+  OrderProcessReducerState
+>;
+// Initial State
+export const orderProcessInitialState: OrderProcessReducerState = {
+  orderProcesses: {
+    data: [],
+    meta: {
+      count: 0,
+      limit: 50,
+      offset: 0
+    }
+  },
+  filterValue: '',
+  isLoading: false,
+  sortBy: {
+    index: 0,
+    property: 'name',
+    direction: 'asc'
+  },
+  selectedOrderProcesses: []
+};
+
+const setLoadingState: OrderProcessReducerActionHandler = (state) => ({
+  ...state,
+  isLoading: true
+});
+const setOrderProcesses: OrderProcessReducerActionHandler = (
+  state,
+  { payload }
+) => ({
+  ...state,
+  orderProcesses: payload,
+  isLoading: false
+});
+const setSortOrderProcesses: OrderProcessReducerActionHandler = (
+  state,
+  { payload }
+) => ({
+  ...state,
+  sortBy: payload,
+  orderProcesses: {
+    ...(state.orderProcesses as ApiCollectionResponse<OrderProcess>),
+    meta: {
+      ...((state.orderProcesses as AnyObject).meta as ApiMetadata),
+      offset: 0
+    }
+  }
+});
+const setFilterValue: OrderProcessReducerActionHandler = (
+  state,
+  { payload }
+) => ({
+  ...state,
+  filterValue: payload,
+  orderProcesses: {
+    ...(state.orderProcesses as ApiCollectionResponse<OrderProcess>),
+    meta: {
+      ...((state.orderProcesses as AnyObject).meta as ApiMetadata),
+      offset: 0
+    }
+  }
+});
+
+export default {
+  [`${FETCH_ORDER_PROCESSES}_PENDING`]: setLoadingState,
+  [`${FETCH_ORDER_PROCESSES}_FULFILLED`]: setOrderProcesses,
+  [SORT_ORDER_PROCESSES]: setSortOrderProcesses,
+  [SET_FILTER_ORDER_PROCESSES]: setFilterValue
+};
