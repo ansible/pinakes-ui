8749bb3430752f23a91ae4096de353c49479d16d
Laura Galis <lgalis@redhat.com>
2020-09-02 17:49:17 -0400

    Reset the sort direction when the filter chips is cleared

--- src/smart-components/portfolio/portfolios.js
+++ src/smart-components/portfolio/portfolios.js
@@ -71,7 +71,7 @@ const portfoliosState = (state, action) => {
         filters: changeFilters(action.payload, state.filterType, state.filters)
       };
     case 'replaceFilterChip':
-      return { ...state, filters: action.payload };
+      return { ...state, sortDirection: SortByDirection.asc, filters: action.payload };
     case 'setFilteringFlag':
       return { ...state, isFiltering: action.payload };
     case 'setFilterType':
