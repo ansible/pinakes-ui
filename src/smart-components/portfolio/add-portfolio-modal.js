329acefc579160ff075178871110180c1b13218a
Martin Maroši <marvusm.mmi@gmail.com>
2020-09-03 15:47:07 +0200

    Use modal form template in add portfolio modal.

--- src/smart-components/portfolio/add-portfolio-modal.js
+++ src/smart-components/portfolio/add-portfolio-modal.js
@@ -1,8 +1,6 @@
 import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
-import { Modal } from '@patternfly/react-core';
-import { Spinner } from '@patternfly/react-core/dist/js/components/Spinner/Spinner';
 import FormRenderer from '../common/form-renderer';
 import { createPortfolioSchema } from '../../forms/portfolio-form.schema';
 import {
@@ -12,7 +10,6 @@ import {
 import useQuery from '../../utilities/use-query';
 import { getPortfolioFromState } from '../../helpers/portfolio/portfolio-helper';
 import useEnhancedHistory from '../../utilities/use-enhanced-history';
-import SpinnerWrapper from '../../presentational-components/styled-components/spinner-wrapper';
 import { UnauthorizedRedirect } from '../error-pages/error-redirects';
 import { PORTFOLIO_ROUTE } from '../../constants/routes';
 import UserContext from '../../user-context';
@@ -77,37 +74,27 @@ const AddPortfolioModal = ({ removeQuery, closeTarget, viewState }) => {
   }
 
   return (
-    <Modal
-      title={
-        portfolioId
+    <FormRenderer
+      schema={createPortfolioSchema(openApiSchema, portfolioId)}
+      onSubmit={onSubmit}
+      onCancel={() => push(closeTarget)}
+      initialValues={{ ...initialValues }}
+      formContainer="modal"
+      modalProps={{
+        title: portfolioId
           ? formatMessage(portfolioMessages.modalEditTitle)
-          : formatMessage(portfolioMessages.modalCreateTitle)
-      }
-      isOpen={isOpen}
-      onClose={() => push(closeTarget)}
-      variant="small"
-    >
-      {!portfolioId || editVariant ? (
-        <FormRenderer
-          schema={createPortfolioSchema(openApiSchema, portfolioId)}
-          schemaType="default"
-          onSubmit={onSubmit}
-          onCancel={() => push(closeTarget)}
-          initialValues={{ ...initialValues }}
-          formContainer="modal"
-          templateProps={{
-            submitLabel: portfolioId
-              ? formatMessage(actionMessages.save)
-              : formatMessage(labelMessages.create),
-            disableSubmit: ['pristine', 'submitting']
-          }}
-        />
-      ) : (
-        <SpinnerWrapper className="pf-u-m-md">
-          <Spinner />
-        </SpinnerWrapper>
-      )}
-    </Modal>
+          : formatMessage(portfolioMessages.modalCreateTitle),
+        isOpen,
+        onClose: () => push(closeTarget),
+        variant: 'small',
+        loading: !portfolioId || editVariant
+      }}
+      templateProps={{
+        submitLabel: portfolioId
+          ? formatMessage(actionMessages.save)
+          : formatMessage(labelMessages.create)
+      }}
+    />
   );
 };
 
