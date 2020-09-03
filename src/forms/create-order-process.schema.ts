b37d3c1ecfbc3980864e9263f7df85c1faa0f453
Martin Maroši <marvusm.mmi@gmail.com>
2020-09-03 12:00:07 +0200

    Migrate create order process schema to TS

--- src/forms/create-order-process.schema.ts
+++ src/forms/create-order-process.schema.ts
@@ -1,18 +1,20 @@
 import componentTypes from '@data-driven-forms/react-form-renderer/dist/cjs/component-types';
 import validatorTypes from '@data-driven-forms/react-form-renderer/dist/cjs/validator-types';
+import Schema from '@data-driven-forms/react-form-renderer/dist/cjs/schema';
 
 import labelMessages from '../messages/labels.messages';
 import debouncedValidatorName from './name-async-validator';
 import orderProcessesMessages from '../messages/order-processes.messages';
 import setItemsSelectSchema from './set-portfolio-item.schema';
 import { BEFORE_TYPE, AFTER_TYPE } from '../utilities/constants';
+import { IntlShape } from 'react-intl';
 
 /**
  * Creates a data-driven-form schema for adding an order processes
  * @param {object} intl React.intl with formatMessage function
  * @param {string} id Id of order process (when editing)
  */
-const createOrderProcessSchema = (intl, id) => {
+const createOrderProcessSchema = (intl: IntlShape, id: string): Schema => {
   return {
     fields: [
       {
@@ -22,7 +24,7 @@ const createOrderProcessSchema = (intl, id) => {
         id: 'order-process-name',
         label: intl.formatMessage(orderProcessesMessages.orderProcessName),
         validate: [
-          (value) => debouncedValidatorName(value, id, intl),
+          (value: string) => debouncedValidatorName(value, id, intl),
           {
             type: validatorTypes.REQUIRED,
             message: intl.formatMessage(
--- src/forms/set-portfolio-item.schema.ts
+++ src/forms/set-portfolio-item.schema.ts
@@ -1,9 +1,14 @@
 import componentTypes from '@data-driven-forms/react-form-renderer/dist/cjs/component-types';
+import Field from '@data-driven-forms/react-form-renderer/dist/cjs/field';
 import loadOptions from './load-items-debounced';
 import formMessages from '../messages/forms.messages';
 import { BEFORE_TYPE } from '../utilities/constants';
+import { IntlShape } from 'react-intl';
 
-const setItemsSelectSchema = (type, intl) => [
+const setItemsSelectSchema = (
+  type: 'before' | 'after',
+  intl: IntlShape
+): Field[] => [
   {
     component: componentTypes.SELECT,
     name: `${type}_portfolio_item_id`,
