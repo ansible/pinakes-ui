00f85a836cc777f1200a182182dfe5509da2e6ad
Martin Maroši <marvusm.mmi@gmail.com>
2020-09-04 10:48:10 +0200

    Migrate approval helper to TS

--- src/helpers/approval/approval-helper.ts
+++ src/helpers/approval/approval-helper.ts
@@ -1,10 +1,27 @@
+import {
+  Workflow,
+  ResourceObject
+} from '@redhat-cloud-services/approval-client';
 import { getWorkflowApi, getAxiosInstance } from '../shared/user-login';
 import { APPROVAL_API_BASE } from '../../utilities/constants';
 import { defaultSettings } from '../shared/pagination';
+import {
+  ApiCollectionResponse,
+  SelectOptions,
+  InternalResourceObject
+} from '../../types/common-types';
 
-export const getApprovalWorkflows = () => getWorkflowApi().listWorkflows();
+export const getApprovalWorkflows = (): Promise<ApiCollectionResponse<
+  Workflow
+>> =>
+  (getWorkflowApi().listWorkflows() as unknown) as Promise<
+    ApiCollectionResponse<Workflow>
+  >;
 
-export const loadWorkflowOptions = (filterValue = '', initialLookup = []) => {
+export const loadWorkflowOptions = (
+  filterValue = '',
+  initialLookup: string[] = []
+): Promise<SelectOptions> => {
   const initialLookupQuery = initialLookup
     .map((workflow) => `filter[id][]=${workflow}`)
     .join('&');
@@ -15,25 +32,35 @@ export const loadWorkflowOptions = (filterValue = '', initialLookup = []) => {
         ''}`
     )
     .then(({ data }) =>
-      data.map(({ id, name }) => ({ label: name, value: id }))
+      data.map(({ id, name }: Workflow) => ({ label: name, value: id }))
     );
 };
 
-export const updateWorkflows = (unlinkIds, linkIds, resourceObject) => {
+export const updateWorkflows = (
+  unlinkIds: string[],
+  linkIds: string[],
+  resourceObject: ResourceObject
+): Promise<void[]> => {
   const unlinkPromises = unlinkIds
     ? unlinkIds.map((wf) => getWorkflowApi().unlinkWorkflow(wf, resourceObject))
     : [];
   const linkPromises = linkIds
     ? linkIds.map((wf) => getWorkflowApi().linkWorkflow(wf, resourceObject))
     : [];
-  return Promise.all([...unlinkPromises, ...linkPromises]);
+  return (Promise.all([
+    ...unlinkPromises,
+    ...linkPromises
+  ]) as unknown) as Promise<void[]>;
 };
 
 export const listWorkflowsForObject = (
-  resourceObject,
-  pagination = { limit: defaultSettings.limit, offset: defaultSettings.offset },
+  resourceObject: InternalResourceObject,
+  pagination: PaginationConfiguration = {
+    limit: defaultSettings.limit,
+    offset: defaultSettings.offset
+  },
   filter = ''
-) => {
+): Promise<ApiCollectionResponse<Workflow>> => {
   const objectQuery = `app_name=${resourceObject.appName}&object_type=${resourceObject.objectType}&object_id=${resourceObject.objectId}`;
   const paginationQuery = `&limit=${pagination.limit}&offset=${pagination.offset}`;
   const filterQuery = `&filter[name][contains]=${filter}`;
