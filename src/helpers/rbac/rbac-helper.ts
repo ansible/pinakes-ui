e9e5ee5568f7fea65b51e2b3fe5bf310f921bbc8
Martin Maroši <marvusm.mmi@gmail.com>
2020-09-04 13:37:33 +0200

    Migrate rbac helper to TS

--- src/helpers/rbac/rbac-helper.ts
+++ src/helpers/rbac/rbac-helper.ts
@@ -1,19 +1,21 @@
 import { getAxiosInstance, getRbacGroupApi } from '../shared/user-login';
 import { RBAC_API_BASE } from '../../utilities/constants';
+import { GroupPagination } from '@redhat-cloud-services/rbac-client';
+import { AxiosPromise } from 'axios';
+import { SelectOptions } from '../../types/common-types';
 
 const api = getRbacGroupApi();
 
-export async function getRbacGroups() {
-  return await api.listGroups();
-}
+export const getRbacGroups = (): AxiosPromise<GroupPagination> =>
+  api.listGroups();
 
-export const fetchFilterGroups = (filterValue = '') =>
+export const fetchFilterGroups = (filterValue = ''): Promise<SelectOptions> =>
   getAxiosInstance()
     .get(
       `${RBAC_API_BASE}/groups/${
         filterValue.length > 0 ? `?name=${filterValue}` : ''
       }`
     )
-    .then(({ data }) =>
+    .then(({ data }: GroupPagination) =>
       data.map(({ uuid, name }) => ({ label: name, value: uuid }))
     );
