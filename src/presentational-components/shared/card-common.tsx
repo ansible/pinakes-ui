9e0f5612c80474196cc23d07c2f024bf9b8dbc3e
Laura Galis <lgalis@redhat.com>
2020-10-13 17:43:41 -0400

    Update card description layout according to the new UX mock

--- src/presentational-components/shared/card-common.tsx
+++ src/presentational-components/shared/card-common.tsx
@@ -2,9 +2,13 @@
 import React, { Fragment } from 'react';
 import styled from 'styled-components';
 import { AnyObject } from '../../types/common-types';
+import Truncate from 'react-truncate';
+import { Tooltip } from '@patternfly/react-core';
 
 const CardPropText = styled.div`
   overflow: hidden;
+  text-overflow: ellipsis;
+  lines: 3;
 `;
 
 export interface ItemDetailsProps extends AnyObject {
@@ -17,7 +21,17 @@ const ItemDetails: React.ComponentType<ItemDetailsProps> = ({
 }) => (
   <Fragment>
     {toDisplay.map((prop) => (
-      <CardPropText key={`card-prop-${prop}`}>{item[prop]}</CardPropText>
+      <Truncate
+        key={`tcard-prop-${prop}`}
+        lines={3}
+        ellipsis={
+          <Tooltip content={<div>{item[prop]}</div>}>
+            <span>...</span>
+          </Tooltip>
+        }
+      >
+        {<CardPropText key={`card-prop-${prop}`}>{item[prop]}</CardPropText>}
+      </Truncate>
     ))}
   </Fragment>
 );
