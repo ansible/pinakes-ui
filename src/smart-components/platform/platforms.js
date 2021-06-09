2ccf67fb5eef2184cbb55cb6fd970561d08283b6
Laura Galis <lgalis@redhat.com>
2021-06-09 09:15:16 -0400

    Add platform doc link to the platforms empty message. Remove Go to sources button

--- src/smart-components/platform/platforms.js
+++ src/smart-components/platform/platforms.js
@@ -1,6 +1,12 @@
-import React, { Fragment, useEffect, useState, useContext } from 'react';
+import React, {
+  Fragment,
+  useEffect,
+  useState,
+  useContext,
+  ReactNode
+} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Button, Text } from '@patternfly/react-core';
+import { Button, Text, TextVariants } from '@patternfly/react-core';
 import { SearchIcon, CogIcon } from '@patternfly/react-icons';
 
 import { scrollToTop } from '../../helpers/shared/helpers';
@@ -14,6 +20,9 @@ import UserContext from '../../user-context';
 import platformsMessages from '../../messages/platforms.messages';
 import useFormatMessage from '../../utilities/use-format-message';
 import filteringMessages from '../../messages/filtering.messages';
+
+const PLATFORMS_DOC_URL =
+  'https://docs.google.com/document/d/1_q4XSHyXtSzddh8maG4w8ZOtnTwebNu8a2XdoTlfCWQ/edit#heading=h.m5qd0ouuw2ek';
 
 const Platforms = () => {
   const formatMessage = useFormatMessage();
@@ -55,19 +64,6 @@ const Platforms = () => {
     isLoading: isLoading && platforms.length === 0
   };
 
-  const NoDataAction = () =>
-    is_org_admin ? (
-      <Button
-        component="a"
-        href={`${document.baseURI}settings/sources`}
-        id="add-source"
-      >
-        {formatMessage(platformsMessages.connectSource)}
-      </Button>
-    ) : (
-      <Text>{formatMessage(platformsMessages.contactAdmin)}</Text>
-    );
-
   const FilterAction = () => (
     <Button
       ouiaId={'clear-filter'}
@@ -79,16 +75,22 @@ const Platforms = () => {
   );
 
   const emptyStateProps = {
-    PrimaryAction:
-      filterValue && filterValue !== '' ? FilterAction : NoDataAction,
+    PrimaryAction: filterValue && filterValue !== '' ? FilterAction : undefined,
     title:
       filterValue && filterValue !== ''
         ? formatMessage(filteringMessages.noResults)
         : formatMessage(platformsMessages.noPlatforms),
     description:
-      filterValue && filterValue !== ''
-        ? formatMessage(filteringMessages.noResultsDescription)
-        : formatMessage(platformsMessages.platformsNoDataDescription),
+      filterValue && filterValue !== '' ? (
+        formatMessage(filteringMessages.noResultsDescription)
+      ) : (
+        <Text id="platform_doc_url" component={TextVariants.p}>
+          {formatMessage(platformsMessages.platformsNoDataDescription)} &nbsp;
+          <a href={PLATFORMS_DOC_URL} target="_blank" rel="noopener noreferrer">
+            {formatMessage(platformsMessages.platformsDocTitle)}
+          </a>
+        </Text>
+      ),
     Icon: filterValue && filterValue !== '' ? SearchIcon : CogIcon
   };
 
