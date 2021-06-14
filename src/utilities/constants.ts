ea7e5cf2e2879eab20f9841d55a94d1e38d73be3
Laura Galis <lgalis@redhat.com>
2021-06-14 08:49:21 -0400

    Fix lint issue

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -33,4 +33,5 @@ export const RETURN_TYPE = 'return';
 
 export const MAX_RETRY_LIMIT = 3;
 export const PLATFORMS_DOC_URL =
+  // eslint-disable-next-line max-len
   'https://access.redhat.com/documentation/en-us/red_hat_ansible_automation_platform/2.0-ea/html-single/red_hat_ansible_automation_platform_installation_guide/index';
