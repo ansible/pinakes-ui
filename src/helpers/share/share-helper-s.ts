30c49dbaf20db7b28bc73d34d01e8f8ac34d5762
Laura Galis <lgalis@redhat.com>
2022-01-04 18:51:55 -0500

    Update share/unshare endpoints for standalone

---
+++ src/helpers/share/share-helper-s.ts
@@ -0,0 +1,51 @@
+import { getAxiosInstance, getPortfolioApi } from '../shared/user-login';
+import { CATALOG_API_BASE } from '../../utilities/constants';
+import {
+  ShareInfo,
+  SharePolicyPermissionsEnum,
+  UnsharePolicyPermissionsEnum
+} from '@redhat-cloud-services/catalog-client';
+
+const axiosInstance = getAxiosInstance();
+const userApi = getPortfolioApi();
+
+export interface SharePolicy {
+  permissions: Array<SharePolicyPermissionsEnum>;
+  groups: Array<string>;
+}
+
+export interface UnsharePolicy {
+  permissions: Array<UnsharePolicyPermissionsEnum>;
+  groups?: Array<string>;
+}
+
+export const getShareInfo = (portfolioId: string): Promise<ShareInfo> =>
+  axiosInstance.get(`${CATALOG_API_BASE}/portfolios/${portfolioId}/share_info`);
+
+export interface ShareData<T = SharePolicyPermissionsEnum> {
+  permissions: T;
+  group_uuid: string;
+  id: string;
+}
+export const sharePortfolio = (data: ShareData): Promise<void> => {
+  const policy: SharePolicy = {
+    permissions: data.permissions.split(',') as SharePolicyPermissionsEnum[],
+    groups: [data.group_uuid]
+  };
+  return axiosInstance.post(
+    `${CATALOG_API_BASE}/portfolios/${data.id}/share/`,
+    policy
+  );
+};
+
+export const unsharePortfolio = (
+  data: ShareData<UnsharePolicyPermissionsEnum[]>
+): Promise<void> => {
+  const policy: UnsharePolicy = {
+    permissions: data.permissions,
+    groups: [data.group_uuid]
+  };
+  return (userApi.unsharePortfolio(data.id, policy) as unknown) as Promise<
+    void
+  >;
+};
