d668dc044ab6fd175903d2d531cf3369d94a6cbd
Laura Galis <lgalis@redhat.com>
2022-01-11 13:32:51 -0500

    Update DDF package. Update router basename.

---
+++ dist_chrome/js/240.8ff3766730c744e2ec85.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcatalog=self.webpackChunkcatalog||[]).push([[240],{20240:(e,n,t)=>{t.d(n,{Z:()=>r});var u=t(30624);const r=function(){var e=(0,u.useRef)(!1);return(0,u.useEffect)((function(){return e.current=!0,function(){return e.current=!1}}),[]),e}}}]);
+//# sourceMappingURL=../sourcemaps/240.3809b615de74b203bebdea594149336d.js.map
