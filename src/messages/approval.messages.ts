ed0a548b8d2ff04ec07ec7e5d075758be3e1c7ed
Laura Galis <lgalis@redhat.com>
2022-02-02 02:47:20 -0500

    Updated styling and messages

--- src/messages/approval.messages.ts
+++ src/messages/approval.messages.ts
@@ -3,13 +3,11 @@ import { defineMessages } from 'react-intl';
 const approvalMessages = defineMessages({
   unlinkNotification: {
     id: 'approval.notifications.unlink',
-    defaultMessage:
-      'Approval processes were unlinked successfully.'
+    defaultMessage: 'Approval processes were unlinked successfully.'
   },
   linkNotification: {
     id: 'approval.notifications.link',
-    defaultMessage:
-      'Approval processes were linked successfully.'
+    defaultMessage: 'Approval processes were linked successfully.'
   },
   setWorkflow: {
     id: 'approval.workflows.set',
