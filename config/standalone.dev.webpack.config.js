56f2a0d38560771588be8ffc87510f470a844ab1
Laura Galis <lgalis@redhat.com>
2022-02-17 15:47:48 -0500

    Updated API path to pinakes

--- config/standalone.dev.webpack.config.js
+++ config/standalone.dev.webpack.config.js
@@ -2,20 +2,20 @@ const webpackBase = require('./webpack.base.config');
 
 // Used for getting the correct host when running in a container
 const proxyHost =
-  process.env.AUTOMATION_SERVICES_CATALOG_API_PROXY_HOST || 'catalog.k8s.local';
-const proxyPort = process.env.AUTOMATION_SERVICES_CATALOG_API_PROXY_PORT || '';
+  process.env.PINAKES_API_PROXY_HOST || 'catalog.k8s.local';
+const proxyPort = process.env.PINAKES_API_PROXY_PORT || '';
 const apiBasePath =
-  process.env.AUTOMATION_SERVICES_CATALOG_API_BASE_PATH ||
-  '/api/automation-services-catalog/v1';
+  process.env.PINAKES_API_BASE_PATH ||
+  '/api/pinakes/v1';
 const authBasePath =
-  process.env.AUTOMATION_SERVICES_CATALOG_AUTH_BASE_PATH ||
-  '/api/automation-services-catalog/auth';
+  process.env.PINAKES_AUTH_BASE_PATH ||
+  '/api/pinakes/auth';
 
 module.exports = webpackBase({
   // The host where the API lives. EX: https://localhost:5001
   API_HOST: '',
 
-  // Path to the API on the API host. EX: /api/automation-services-catalog
+  // Path to the API on the API host. EX: /api/pinakes
   API_BASE_PATH: apiBasePath,
   AUTH_BASE_PATH: authBasePath,
 
--- config/standalone.prod.webpack.config.js
+++ config/standalone.prod.webpack.config.js
@@ -3,8 +3,8 @@ const webpackBase = require('./webpack.base.config');
 // Compile configuration for stnadalone mode
 module.exports = webpackBase({
   API_HOST: '',
-  API_BASE_PATH: '/api/automation-services-catalog/v1',
-  AUTH_BASE_PATH: '/api/automation-services-catalog/auth',
+  API_BASE_PATH: '/api/pinakes/v1',
+  AUTH_BASE_PATH: '/api/pinakes/auth',
   DEPLOYMENT_MODE: 'standalone',
   UI_USE_HTTPS: false,
   UI_DEBUG: false,
