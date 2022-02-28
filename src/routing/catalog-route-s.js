be28677c6c11e82febc6463932ee5586a889260b
Laura Galis <lgalis@redhat.com>
2022-02-28 11:13:31 -0500

    Redirect to login on 403 for the me endpoint

--- src/routing/catalog-route-s.js
+++ src/routing/catalog-route-s.js
@@ -12,6 +12,12 @@ const CatalogRoute = ({ ...props }) => {
       })
       .catch((error) => {
         localStorage.removeItem('catalog_user');
+        console.log('Debug - error: ', error);
+        if (error.status === 403) {
+          window.location.replace(`${AUTH_API_BASE}/login/`);
+          return <div />;
+        }
+
         throw error;
       });
   }, []);
