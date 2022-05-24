0a1730d2941def8818b34bbb70825c4689ddd62c
Laura Galis <lgalis@redhat.com>
2022-05-24 15:08:46 -0400

    Add notification settings selections to the create/edit template modals

--- src/smart-components/template/add-template-modal.js
+++ src/smart-components/template/add-template-modal.js
@@ -19,6 +19,7 @@ const AddTemplate = ({ postMethod, pagination = defaultSettings }) => {
   const intl = useIntl();
 
   const onSave = ({ ...values }) => {
+    console.log('Debug - values', values);
     return dispatch(
       addTemplate(
         {
