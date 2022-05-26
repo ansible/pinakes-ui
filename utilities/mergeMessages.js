9aed54d2682383fc2a5a1795b43961db0d78a477
Laura Galis <lgalis@redhat.com>
2022-05-26 17:31:50 -0400

    Localization - add locale selection

--- utilities/mergeMessages.js
+++ utilities/mergeMessages.js
@@ -8,7 +8,7 @@ const last = require('lodash/last');
 
 let MESSAGES_PATTERN = 'build/messages/**/*.json';
 let LANG_DIR = 'locales/';
-let LANG_PATTERN = 'locales/**/messages.json';
+let LANG_PATTERN = '';
 let IGNORED = ['translations', 'data'];
 
 program
@@ -37,30 +37,18 @@ if (program.langDir) {
 
 if (program.langPattern) {
   LANG_PATTERN = `${LANG_DIR}${program.langPattern}`;
+} else {
+  LANG_PATTERN = `${LANG_DIR}/*.json`;
 }
 
 // Merge translated json files (es.json, fr.json, etc) into one object
 // so that they can be merged with the eggregated 'en' object below
-const localeMessages = (filename) => {
-  const file = fs.readFileSync(filename, 'utf8');
-  const messages = JSON.parse(file);
-  let collection = {};
-  Object.keys(messages).forEach((key) => {
-    if (collection.hasOwnProperty(key)) {
-      throw new Error(`Duplicate message id: ${key}`);
-    }
-
-    collection[key] = messages[key].defaultMessage;
-  });
-  return collection;
-};
 
 const mergedTranslations = globSync(`${rootFolder}${LANG_PATTERN}`)
   .map((filename) => {
-    const locale = filename.split('/').reverse()[1];
-    const file = last(filename.split('/'));
-    if (!IGNORED.includes(file)) {
-      return { [locale]: localeMessages(filename) };
+    const locale = last(filename.split('/')).split('.json')[0];
+    if (!IGNORED.includes(locale)) {
+      return { [locale]: JSON.parse(fs.readFileSync(filename, 'utf8')) };
     }
   })
   .reduce((acc, localeObj) => {
@@ -71,16 +59,17 @@ const mergedTranslations = globSync(`${rootFolder}${LANG_PATTERN}`)
 // React components via the React Intl Babel plugin. An error will be thrown if
 // there are messages in different components that use the same `id`. The result
 // is a flat collection of `id: message` pairs for the app's default locale.
+
 const defaultMessages = globSync(`${rootFolder}${MESSAGES_PATTERN}`)
   .map((filename) => fs.readFileSync(filename, 'utf8'))
   .map((file) => JSON.parse(file))
   .reduce((collection, descriptors) => {
-    Object.keys(descriptors).forEach((key) => {
-      if (collection.hasOwnProperty(key)) {
-        throw new Error(`Duplicate message id: ${key}`);
+    descriptors.forEach(({ id, defaultMessage }) => {
+      if (collection.hasOwnProperty(id)) {
+        throw new Error(`Duplicate message id: ${id}`);
       }
 
-      collection[key] = descriptors[key].defaultMessage;
+      collection[id] = defaultMessage;
     });
     return collection;
   }, {});
