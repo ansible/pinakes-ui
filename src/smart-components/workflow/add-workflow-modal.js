496820fbe84b7a03e11d018d22ffda0d41d10d5a
Laura Galis <lgalis@redhat.com>
2022-06-09 10:00:13 -0400

    Remove checking the name for unefined

--- src/smart-components/workflow/add-workflow-modal.js
+++ src/smart-components/workflow/add-workflow-modal.js
@@ -11,7 +11,6 @@ import FormRenderer from '../common/form-renderer';
 import addWorkflowSchema from '../../forms/add-workflow.schema';
 import formMessages from '../../messages/form.messages';
 import { defaultSettings } from '../../helpers/shared/approval-pagination';
-import { listTemplates } from '../../helpers/workflow/workflow-helper';
 
 const reducer = (state, { type }) => {
   switch (type) {
@@ -35,12 +34,10 @@ const AddWorkflow = ({ postMethod, pagination = defaultSettings }) => {
   });
 
   useEffect(() => {
-    listTemplates().then((data) =>
-      stateDispatch({
-        type: 'loaded',
-        schema: addWorkflowSchema(intl, data.data)
-      })
-    );
+    stateDispatch({
+      type: 'loaded',
+      schema: addWorkflowSchema(intl)
+    });
   }, []);
 
   const onSave = ({ group_refs = [], ...values }) => {
