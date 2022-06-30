059a5031fa8d0c5924952c317057e591912dba6a
Aditya Mulik <aditya.mulik@gmail.com>
2022-06-30 07:56:19 -0400

    final translation shell scripts

---
+++ tools/scripts/l18n/post_translation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd _clones/pinakes-ui/
+
+# Rename the zh_cn folder 
+mv translations/zh_cn translations/zh
+
+# locale will be dropped here
+pinakes_ui_path="locales" 
+
+# Copy and update the files
+rsync -av translations/ $pinakes_ui_path
+
+# Cleanup
+rm -rf translations/
